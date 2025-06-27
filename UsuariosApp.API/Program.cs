3c9e0be6c1f028528157e546da31d60ff22706e5
AlMouta <almouta@hotmail.com>
2025-06-27 20:16:04 -0300

    Adicionar arquivos de projeto.

---
+++ UsuariosApp.API/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using UsuariosApp.API.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddRouting(map => map.LowercaseQueryStrings = true);
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Injeção de dependência para o DataContext (Entity Framework)
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("UsuariosApp"))
+    );
+
+var app = builder.Build();
+
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapScalarApiReference(s => s.WithTheme(ScalarTheme.BluePlanet)); //Scalar
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
